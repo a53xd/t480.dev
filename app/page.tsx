8512e98fe1ca7ae5f8a722b929e42f349a55296a
charlie <a53x@shitposting.expert>
2025-01-11 12:07:53 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        hey, i'm charlie.
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Hey! I'm the founder and owner of loopback, and i'm a full stack dev based in Lithuania.
+        I really like developing open source stuff and i really like opensource overall. I'm non-binary, and i'm also a journalist.
+        I'm also the owner of the Human Foundation, which is a group of companies ethically working for the people. any pronouns.`}
       </p>
       <div className="my-8">
         <BlogPosts />
