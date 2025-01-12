8fa599c39dd7608c2289b1625c7099d52472722f
humanoid Apollo <txfnc@dmc.chat>
2025-01-12 12:43:25 +0200

    balls

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import { BlogPosts } from 'app/components/posts'
+<a rel="me" href="https://hachyderm.io/@imcharlie">Mastodon</a>
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        error: component load fail
+        heya, name's charlie.
       </h1>
       <p className="mb-4">
-        {`error: component load fail`}
+        {`I'm the founder and owner of mosaic, and i'm a full stack dev based in Lithuania.
+I really like developing open source stuff and i really like opensource overall. I'm non-binary, and i'm also a journalist.
+I'm also the owner of the Mosaic Foundation, which is a foundation trying to reclaim the internet, and make it free of big tech. any pronouns.`}
       </p>
       <div className="my-8">
         <BlogPosts />
