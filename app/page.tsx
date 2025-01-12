997abec52343871e2e67aca7c039dbc1a7aaf7fd
charlie <a53x@shitposting.expert>
2025-01-12 10:49:14 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <p className="mb-4">
         {`Hey! I'm the founder and owner of loopback, and i'm a full stack dev based in Lithuania.
         I really like developing open source stuff and i really like opensource overall. I'm non-binary, and i'm also a journalist.
-        I'm also the owner of the Human Foundation, which is a group of companies ethically working for the people. any pronouns.`}
+        I'm also the owner of the Mosaic Foundation, which is a foundation trying to reclaim the internet, and make it free of big tech. any pronouns.`}
       </p>
       <div className="my-8">
         <BlogPosts />
