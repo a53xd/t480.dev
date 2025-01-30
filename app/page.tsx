c721ff99e4af684ad6bd07187d945a89c6ff7a55
charlie <a53x@shitposting.expert>
2025-01-30 21:04:14 +0200

    though is this better

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Page() {
       <p className="mb-4">
         {`I'm the founder and owner of mosaic, and i'm a full stack dev based in Lithuania.
 I really like developing open source stuff and i really like opensource overall. I'm non-binary, and i'm also a journalist.
-I'm also the owner of the Mosaic Foundation, which is a foundation trying to reclaim the internet, and make it free of big tech. any pronouns.
-`}
+I'm also the owner of the Mosaic Foundation, which is a foundation trying to reclaim the internet, and make it free of big tech. any pronouns.`}
+        </p>
         <a href="https://t480.dev/donations" target="_blank" rel="noopener noreferrer">
           If you want, you can send me some monero, or buy me a coffee.
         </a>
