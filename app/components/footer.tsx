42706247adbb1b2183a749d3c7fbbb1ddd4182f7
charlie <a53x@shitposting.expert>
2025-01-30 14:20:52 +0200

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -57,17 +57,6 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://hachyderm.io/@imcharlie"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">mastodon</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
             href="https://ko-fi.com/chardev"
           >
             <ArrowIcon />
